49ceb9e93e42daafc103eddf8bacbdd538675852
SSY <songsy0203ai@gmail.com>
2026-03-18 13:28:51 +0900

    Update finding_llm.py

--- 03_system_monitor/ssy_system_monitor_v12_LLM/finding_llm.py
+++ 03_system_monitor/ssy_system_monitor_v12_LLM/finding_llm.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # 서영님의 키 입력
-genai.configure(api_key="AIzaSyAUxxmUwxB1rR_jFob0zIp56I1Q5vquXJ4") 
+genai.configure(api_key="MY_KEY") 
 
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
